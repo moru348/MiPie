c43e99716e418a85e4a75b334a84c608dff4c306
moru3 <moru348@gmail.com>
2020-12-19 19:10:18 +0900

    update

--- src/main/java/me/moru3/mipie/Parabola.java
+++ src/main/java/me/moru3/mipie/Parabola.java
@@ -1,7 +1,11 @@
 package me.moru3.mipie;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.plugin.Plugin;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Parabola {
     double height;
@@ -18,13 +22,18 @@ public class Parabola {
         ph_const = height/Math.pow(width/2, 2);
     }
 
-    public void start(Entity entity) {
-        Location location = start;
-        for(double j = -(width/2.0);j<=width/2;j+=0.1) {
-            location = new Location(start.getWorld(), start.getX()+(height/100)/(width/100),
-                    start.getY()+(ph_const*(start.getY() + end.getY()) * j - ph_const*start.getY()*end.getY()),
-                    start.getX()+(width/100)/(height/100));
-            entity.teleport(location);
-        }
+    public void start(Entity entity, Plugin plugin) {
+        AtomicReference<Location> location = new AtomicReference<>(start);
+        AtomicReference<Double> j = new AtomicReference<>(-(width/2.0));
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+            if(j.get()<=width/2) {
+                return;
+            }
+            location.set(new Location(start.getWorld(), start.getX() + (height / 100) / (width / 100),
+                    start.getY() + (ph_const * (start.getY() + end.getY()) * j.get() - ph_const * start.getY() * end.getY()),
+                    start.getX() + (width / 100) / (height / 100)));
+            entity.teleport(location.get());
+            j.updateAndGet(i -> i++);
+        }, 0, 1);
     }
 }
